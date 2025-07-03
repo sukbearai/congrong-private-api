2c012d81b3eaac45c761ac447f35780f1ba3b097
suk.bear <suk.bear.suwu@gmail.com>
2025-07-03 13:12:47 +0800

    feat: add schema index file to export all database tables for improved organization

---
+++ apps/congrong-private-api/db/schema/index.ts
@@ -0,0 +1,4 @@
+// 导出所有表
+export { usersTable } from './user'
+export { productsTable } from './product'
+export { snapshootTable } from './snapshoot'
--- apps/congrong-private-api/server/utils/db.ts
+++ apps/congrong-private-api/server/utils/db.ts
@@ -2,8 +2,8 @@ export { and, eq, or, sql } from 'drizzle-orm'
 export type { DrizzleD1Database } from 'drizzle-orm/d1'
 export { drizzle } from 'drizzle-orm/d1'
 export { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+// 不要删下面这行代码，会导致生产报错 schema is not defined
+export * as schema from '~~/db/schema'
 export { usersTable } from '~~/db/schema/user'
-// 不要删这行代码，会导致网络cors错误
-export * as schema from '~~/db/schema/user'
 export { productsTable } from '~~/db/schema/product'
 export type { TokenData, WechatApiError, WechatTokenResponse } from '~~/types/'
