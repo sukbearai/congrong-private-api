432a01582eac21ed6669e47da8245b70b620535b
suk.bear <suk.bear.suwu@gmail.com>
2025-07-03 12:07:59 +0800

    fix: correct export statement for user schema to prevent CORS issues

--- apps/congrong-private-api/server/utils/db.ts
+++ apps/congrong-private-api/server/utils/db.ts
@@ -3,6 +3,7 @@ export type { DrizzleD1Database } from 'drizzle-orm/d1'
 export { drizzle } from 'drizzle-orm/d1'
 export { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 export { usersTable } from '~~/db/schema/user'
-export * as userSchema from '~~/db/schema/user'
+// 不要删这行代码，会导致网络cors错误
+export * as schema from '~~/db/schema/user'
 export { productsTable } from '~~/db/schema/product'
 export type { TokenData, WechatApiError, WechatTokenResponse } from '~~/types/'
