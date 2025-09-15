90cd528db38355a32ef9d1178c984ed922620560
suk.bear <suk.bear.suwu@gmail.com>
2025-09-15 09:54:04 +0800

    fix: update greeting message to include fire emoji

--- apps/congrong-private-api/server/routes/index.ts
+++ apps/congrong-private-api/server/routes/index.ts
@@ -1,4 +1,4 @@
 export default eventHandler(async (event) => {
   setResponseHeader(event, 'Content-Type', 'text/plain; charset=utf-8')
-  return 'Hello World,I am Suk.Bear!😊'
+  return 'Hello World,I am Suk.Bear!😊🔥'
 })
